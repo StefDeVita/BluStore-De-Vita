b3d3b3c355382ac4ce338a79555cd4d515ea5cc6
StefDeVita <devitastefano@hotmail.com>
2021-09-05 21:27:58 -0300

    Borro elementos default de React y hago la navbar para el primer desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-light">
+            <h2 className="navbar-brand">Blu-Store</h2>
+            <a className="nav-item" href="">Inicio</a>
+            <a className="nav-item" href="">Productos</a>
+            <a className="nav-item" href="">Acerca de</a>
+                
+            
+            
+        </nav>
+    )
+}
+
+export default NavBar
+
